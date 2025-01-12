f96d235f6a18a920eea5699eb33292a982d22c5f
al asyari <yusrulazizi59@gmail.com>
2025-01-13 05:24:05 +0800

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,27 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
+    "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: { },
-  },
+
   plugins: [
     require('daisyui'),
   ],
- 
- 
 }
+// export default {
+//   content: [
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: { },
+//   },
+//   plugins: [
+//     require('daisyui'),
+//   ],
+ 
+ 
+// }
 
 
 
