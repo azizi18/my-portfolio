142a8e005a932f2ba601e122a3a8b6eab4bde66e
al asyari <yusrulazizi59@gmail.com>
2025-01-13 21:08:27 +0800

    send to emailjs

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
-import profil from '../assets/azizi.png';
+import React, { useRef, useState }  from 'react'
+import emailjs from '@emailjs/browser';
 
 
 const Contact = () => {
+  const form = useRef();
+  const [alertMessage, setAlertMessage] = useState('');
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_9p9ybae', 'template_stidunl', form.current, {
+        publicKey: 'ZsePwT47FzJwoyd43',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          setAlertMessage('Message successfully!');
+          form.current.reset(); 
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+          setAlertMessage('Failed. Please try again later.');
+        },
+      );
+  };
     return (
  <div className="py-12 bg-base-100 " id='contact'>
     <h2 className="text-3xl text-grey font-bold text-center mb-4">Contact</h2>
@@ -31,8 +52,32 @@ const Contact = () => {
       </div>
    
    <div className="card bg-neutral text-neutral-content w-96">
+   {alertMessage && <div className="alert alert-success">
+    <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6 shrink-0 stroke-current"
+    fill="none"
+    viewBox="0 0 24 24">
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+  </svg>{alertMessage}</div>}
+   <form ref={form} onSubmit={sendEmail}>
   <div className="card-body">
     <h2 className="card-title">Contact</h2>
+<label className="input input-bordered flex items-center gap-2">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 16 16"
+    fill="currentColor"
+    className="h-4 w-4 opacity-70">
+    <path
+      d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+  </svg>
+  <input type="text" className="grow" placeholder="Name" name='user_name' />
+</label>
 <label className="input input-bordered flex items-center gap-2">
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -44,24 +89,14 @@ const Contact = () => {
     <path
       d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
   </svg>
-  <input type="text" className="grow" placeholder="Email" />
+  <input type="text" className="grow" placeholder="Email" name='user_email' />
 </label>
-<label className="input input-bordered flex items-center gap-2">
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 16 16"
-    fill="currentColor"
-    className="h-4 w-4 opacity-70">
-    <path
-      d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-  </svg>
-  <input type="text" className="grow" placeholder="Name" />
-</label>
-<textarea className="textarea textarea-accent" placeholder="Give criticism and suggestions....."></textarea>
+<textarea className="textarea textarea-accent" placeholder="Give criticism and suggestions....." name='message'></textarea>
     <div className="card-actions py-5 justify-end">
-      <button className="btn text-[#27323E] font-reguler  bg-gradient-to-r from-[#06B7CF] to-[#96FFC6]  hover:from-slate-500 hover:via-slate-600 hover:to-slate-400 space-x-2 transform transition duration-300 ease-in-out">Send</button>
+      <button type="submit" value="Send" className="btn text-[#27323E] font-reguler  bg-gradient-to-r from-[#06B7CF] to-[#96FFC6]  hover:from-slate-500 hover:via-slate-600 hover:to-slate-400 space-x-2 transform transition duration-300 ease-in-out">Send</button>
     </div>
   </div>
+     </form>
 </div>
    </div>
        </div>
